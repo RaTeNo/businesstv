61593086ad62e75cd4870132b23535888a731163
akella <akella.a@gmail.com>
2013-09-14 23:05:29 +0300

    responsive

--- js/common.js
+++ js/common.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 
+
+
+
+
+
+	//begin debug width
+	$('body').after('<div class="debug"></div>')
+	$(window).on( 'resize', debug);
+	function debug(){
+		$('.debug').html($(window).width());
+	}
+	debug();
+	//end debug width
 });
