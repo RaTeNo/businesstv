e798799bf84dd9b7f1c95efe32bf8816fea8718a
akella <akella.a@gmail.com>
2013-09-16 01:14:22 +0300

    main with menu

--- js/common.js
+++ js/common.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
 
 
 	//begin debug width
-	$('body').after('<div class="debug"></div>')
-	$(window).on( 'resize', debug);
-	function debug(){
-		$('.debug').html($(window).width());
-	}
-	debug();
+	// $('.page').after('<div class="debug"></div>')
+	// $(window).on( 'resize', debug);
+	// function debug(){
+	// 	$('.debug').html($(window).width());
+	// }
+	// debug();
 	//end debug width
 });
