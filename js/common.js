32862253af74bdab282d7327d62cabde86d50413
Splintex <godun.yevgen@gmail.com>
2013-10-17 17:04:45 +0300

    new pages

--- js/common.js
+++ js/common.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
 
-	if($(".tv-program__top").length>0){
-		$(".tv-program__top").scrollable();
-		var ap = $(".tv-program__top").data('scrollable');
+	// if($(".tv-program__top").length > 0){
+	// 	$(".tv-program__top").scrollable();
+	// 	var ap = $(".tv-program__top").data('scrollable');
   		
   	
 
-  		ap.onSeek(function(event, index) {
-  			cornum = Math.floor($('.tv-program').width()/100);
-		    if (this.getIndex() >= this.getSize() - cornum) {    
-		      $('.move__next').addClass('disabled');
-		    }
-		  });
+ //  		ap.onSeek(function(event, index) {
+ //  			cornum = Math.floor($('.tv-program').width()/100);
+	// 	    if (this.getIndex() >= this.getSize() - cornum) {    
+	// 	      $('.move__next').addClass('disabled');
+	// 	    }
+	// 	  });
 
-  		$(window).on( 'resize', checkslider);
-		function checkslider(){
-			if($('.tv-program').width()>700){
-				$('.move__next,.move__prev').addClass('disabled');
-			}
-			else{
-				$('.move__next').removeClass('disabled');
-			}
-		}
-		checkslider();
-	}
+ //  		$(window).on( 'resize', checkslider);
+	// 	function checkslider(){
+	// 		if($('.tv-program').width()>700){
+	// 			$('.move__next,.move__prev').addClass('disabled');
+	// 		}
+	// 		else{
+	// 			$('.move__next').removeClass('disabled');
+	// 		}
+	// 	}
+	// 	checkslider();
+	// }
 	$('.header__toggle').click(function () {
 		$('body').toggleClass("is-withmenu");
 	});
@@ -32,26 +32,26 @@ $(document).ready(function() {
 		$('body').toggleClass("is-withsearch");
 	});
 
-	// program tooltip
-	// function tv_tooltip() {
-	// 	var tv_program = $(".js-tv-program li")
-	// 	tv_program.click(function(){
+	//program tooltip
+	function tv_tooltip() {
+		var tv_program = $(".js-tv-program li")
+		tv_program.click(function(){
 
-	// 		var program_info = $(this).find(".js-prog-info").html();
+			var program_info = $(this).find(".js-prog-info").html();
 
-	// 		var tooltip = $(".js-tooltip-program");
-	// 		tooltip.html(program_info).show();
-	// 		// tooltip positin
-	// 		var pos_top = $(this).position().top + 45;
-	// 		var pos_left = $(this).position().left +48;
-	// 		tooltip.css({
-	// 			"top": pos_top,
-	// 			"left": pos_left,
-	// 		});
-	// 		return false;
-	// 	});
-	// }
-	// tv_tooltip();
+			var tooltip = $(".js-tooltip-program");
+			tooltip.html(program_info).show();
+			// tooltip positin
+			var pos_top = $(this).position().top + 45;
+			var pos_left = $(this).position().left +48;
+			tooltip.css({
+				"top": pos_top,
+				"left": pos_left,
+			});
+			return false;
+		});
+	}
+	tv_tooltip();
 	$(window).resize(function(){
 		tv_tooltip();
 	});
